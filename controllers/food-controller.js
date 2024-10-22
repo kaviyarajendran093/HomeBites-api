c82599a9081cabe169e77ea49bd95fb2b2226c15
kaviyaRaj <kaviyaraj1193@gmail.com>
2024-10-22 15:33:22 -0400

    add the endpoints to get all the cuisine data from database

--- controllers/food-controller.js
+++ controllers/food-controller.js
@@ -16,4 +16,37 @@ const getAllCategory = async (req, res) => {
   }
 };
 
-export { getAllCategory };
+const getAllCuisine = async (req, res) => {
+  try {
+    const data = await knex("cuisine_master");
+    const responseJson = data.map((value) => {
+      const { created_at, updated_at, ...rest } = value;
+      return rest;
+    });
+    res.status(200).json(responseJson);
+  } catch (error) {
+    res.status(500).send("Error retrieving cuisine items");
+  }
+};
+
+const getCuisineByCategoryId = async (req, res) => {
+  try {
+    const categoryId = req.params.category_id;
+    const data = await knex("cuisine_master as c")
+      .distinct(
+        "c.cuisine_id",
+        "c.cuisine_name",
+        "c.image_url",
+        "c.description"
+      )
+      .join("food as f", "f.cuisine_id", "c.cuisine_id")
+      .where("f.category_id", categoryId)
+      .orderBy("c.cuisine_id");
+
+    res.status(200).json(data);
+  } catch (error) {
+    res.status(500).send("Error retrieving cuisine items");
+  }
+};
+
+export { getAllCategory, getAllCuisine, getCuisineByCategoryId };
--- routes/food.js
+++ routes/food.js
@@ -1,8 +1,16 @@
 import express from "express";
-import { getAllCategory } from "../controllers/food-controller.js";
+import {
+  getAllCategory,
+  getAllCuisine,
+  getCuisineByCategoryId,
+} from "../controllers/food-controller.js";
 
 const foodRoute = express.Router();
 
 foodRoute.route("/").get(getAllCategory);
+
+foodRoute.route("/cuisine").get(getAllCuisine);
+
+foodRoute.route("/cuisine/:category_id").get(getCuisineByCategoryId);
 
 export default foodRoute;
